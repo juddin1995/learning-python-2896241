56b320d47e92bc57cf32ead23751a5d60de5e389
jomir <jomir.uddin112@gmail.com>
2024-12-02 05:07:52 -0500

    Hello World Python3 Complete

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,10 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    print("Hello, World!")
+    name = input("What is your name?")
+    print("Nice to meet youm ", name)
 
+if __name__ == "__main__":
+    main()
